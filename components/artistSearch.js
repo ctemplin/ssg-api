ctemplin/ssg-api

commit 99aa99edfc2dc8457bd900ef18f161d7a2e47b8b

Small fixes, whitespace, etc

components/artistSearch.js
@@ -9,8 +9,7 @@ export default function ArtistSearch({
   data, setData, 
   searchTerms, setSearchTerms,
   scrollTop, setSearchScroll,
-  hlIndex, setHlIndex
-}) {
+  hlIndex, setHlIndex}) {
 
   useEffect(() => {
     const getData = async () => {
@@ -188,7 +187,7 @@ export default function ArtistSearch({
       />
       <div className={styles.inputContainer}>
         <input type="text" onKeyDown={handleKeyDown} onChange={handleChange} ref={inputRef} className={styles.input} defaultValue={searchTerms} placeholder="Artist search..."/>
-        {data.matches && data.matches.length ?
+        {data.matches?.length > 0 ?
           <div className={styles.searchIncResultList} id="searchIncResultList">
           {data.matches.map((_,i) =>
           <div className={`${styles.searchIncResult} ${hlIndex==i && styles.searchIncResultHl}`} index={i} rid={_.id} key={_.id}
@@ -198,14 +197,9 @@ export default function ArtistSearch({
           )}
           </div>
         :
-          <></>
-        }
-        {data.matches && data.matches.length == 0 ?
           <div className={styles.searchIncResultList} id="searchIncResultList">
-            <div className={`${styles.searchIncResult} ${styles.searchIncResultWarn} panel-block`}>No results found</div>
+            <div className={`${styles.searchIncResult} ${styles.searchIncResultWarn}`}>No results found</div>
           </div>
-        :
-          <></>
         }
       </div>
     </div>

netlify/functions/youtube-video-search.js
@@ -1,5 +1,3 @@
-const { faUserLock } = require('@fortawesome/free-solid-svg-icons')
-
 exports.handler = async function(event, context) {
 
   const fetch = require('node-fetch')

pages/index.js
@@ -108,7 +108,7 @@ export default function Home({aid}) {
           overflow-y: overlay !important;
         }`}</style>
       </Head>
-        {router.query.aid &&
+      {router.query.aid &&
         <div className={styles.columns}>
           <div className={styles.column}>
             <a onClick={handleSearchClick}>
@@ -126,25 +126,25 @@ export default function Home({aid}) {
             </a>
           </div>
         </div>
-        }
-        {!router.query.aid &&
-          <>
-          <div>{/* first grid row. reserved for header/menu */}</div>
-          <div className={styles.artistSearchContainer}>
-            <ArtistSearch
-              defaultData={defaultSearchData}
-              handleArtistSearchClick={handleArtistSearchClick}
-              data={searchData} setData={setSearchData}
-              searchTerms={searchTerms} setSearchTerms={setSearchTerms}
-              scrollTop={searchScroll} setSearchScroll={setSearchScroll}
-              hlIndex={searchHlIndex} setHlIndex={setSearchHlIndex}
-            />
-            <Image src="/headphones.svg" className={styles.headphones} alt="" layout="fill" preload="true"/>
-
-          </div>
-          </>}
-        {router.query.aid &&
+      }
+      {!router.query.aid &&
         <>
+        <div>{/* first grid row. reserved for header/menu */}</div>
+        <div className={styles.artistSearchContainer}>
+          <ArtistSearch
+            defaultData={defaultSearchData}
+            handleArtistSearchClick={handleArtistSearchClick}
+            data={searchData} setData={setSearchData}
+            searchTerms={searchTerms} setSearchTerms={setSearchTerms}
+            scrollTop={searchScroll} setSearchScroll={setSearchScroll}
+            hlIndex={searchHlIndex} setHlIndex={setSearchHlIndex}
+          />
+          <Image src="/headphones.svg" className={styles.headphones} alt="" layout="fill" preload="true"/>
+
+        </div>
+        </>}
+      {router.query.aid &&
+        <>  
         <div className={styles.columns}>
           <div className={styles.column}>
             <Artist id={router.query.aid} handleReleaseGroupClick={handleReleaseGroupSelect}/>
@@ -167,14 +167,11 @@ export default function Home({aid}) {
         {curTrackId &&
         <Recording id={curTrackId} handleMaxClick={handleMaxClick} isMaxed={trackMaxed}></Recording>
         }
-
         {coverArtId &&
         <CoverArt id={coverArtId} handleCoverArtSmall={handleCoverArtSmall} handleCloseClick={hideLargeImg} showLargeImg={showLargeImg}></CoverArt>
         }
         </>
-        }
-
-      {true &&
+      }
       <footer className={styles.footer}>
         <FontAwesomeIcon
           className={styles.icon}
@@ -184,7 +181,7 @@ export default function Home({aid}) {
         <span>
           Made with <a href="https://nextjs.org">NextJS</a> and the <a href="https://musicbrainz.org/doc/MusicBrainz_API">MusicBrainz API</a>
         </span>
-      </footer>}
+      </footer>
     </div>
   )
 }

commit 7b2d17a215b49f1bd1d37813d34d172c7f8fd70a

Fix display glitch introduced in 99aa99e.

components/artistSearch.js
@@ -187,19 +187,23 @@ export default function ArtistSearch({
       />
       <div className={styles.inputContainer}>
         <input type="text" onKeyDown={handleKeyDown} onChange={handleChange} ref={inputRef} className={styles.input} defaultValue={searchTerms} placeholder="Artist search..."/>
-        {data.matches?.length > 0 ?
-          <div className={styles.searchIncResultList} id="searchIncResultList">
-          {data.matches.map((_,i) =>
-          <div className={`${styles.searchIncResult} ${hlIndex==i && styles.searchIncResultHl}`} index={i} rid={_.id} key={_.id}
-            onClick={handleClick(_.id, _.name)} onKeyDown={handleKeyDown} onMouseEnter={handleMouseEnter} onFocus={handleMouseEnter} tabIndex="0">
-            {_.name}
-          </div>
-          )}
-          </div>
-        :
-          <div className={styles.searchIncResultList} id="searchIncResultList">
-            <div className={`${styles.searchIncResult} ${styles.searchIncResultWarn}`}>No results found</div>
-          </div>
+        {data.matches &&
+        <>
+          {data.matches?.length > 0 ?
+            <div className={styles.searchIncResultList} id="searchIncResultList">
+            {data.matches.map((_,i) =>
+            <div className={`${styles.searchIncResult} ${hlIndex==i && styles.searchIncResultHl}`} index={i} rid={_.id} key={_.id}
+              onClick={handleClick(_.id, _.name)} onKeyDown={handleKeyDown} onMouseEnter={handleMouseEnter} onFocus={handleMouseEnter} tabIndex="0">
+              {_.name}
+            </div>
+            )}
+            </div>
+          :
+            <div className={styles.searchIncResultList} id="searchIncResultList">
+              <div className={`${styles.searchIncResult} ${styles.searchIncResultWarn}`}>No results found</div>
+            </div>
+          }
+        </>
         }
       </div>
     </div>