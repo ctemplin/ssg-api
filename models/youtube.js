90912168dacb98489d9a36ae01d7c7e381f68617
Templin, Chris <ctemplin@gmail.com>
2021-08-08 12:41:06 -0400

    Caching youtube results.

--- models/youtube.js
+++ models/youtube.js
@@ -1,4 +1,4 @@
-import { atom, selectorFamily } from 'recoil'
+import { atom, selector, selectorFamily } from 'recoil'
 import * as data from '../data/youtube'
 import { currentArtistAtom, currentRecordingAtom } from './musicbrainz'
 
@@ -14,14 +14,25 @@ export const youtubeVideoResults = atom({
   }
 })
 
-// Using SelectorFamily for consistency even though is is un-parametized
+const youtubeQueryString = selector({
+  'key': 'youtubeQueryString',
+  get: ({get}) => {
+    const rec = get(currentRecordingAtom)
+    const artist = get(currentArtistAtom)
+    if (!rec.id) return null
+    return `${rec.title} ${artist.name}`
+  }
+})
+
+// Using SelectorFamily for consistency even though is is un-parametized.
+// Using selector above for querystring to leverage caching. Directly getting
+// resource atoms would make the dependencies too granular.
 export const youtubeVideoSearch = selectorFamily({
   'key': 'youtubeSearch',
   get: () => async({get}) => {
-    const rec = get(currentRecordingAtom)
-    const artist = get(currentArtistAtom)
-    if (!rec.id) return get(youtubeVideoResults)
-    const params = [['q', `${rec.title} ${artist.name}`]]
+    const qs = get(youtubeQueryString)
+    if (!qs) return get(youtubeVideoResults)
+    const params = [['q', qs]]
     return await data.youtubeVideoSearch(params)
   }
 })
