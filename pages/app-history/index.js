f3560e434fca5a17cba23aad65144876cba4975a
Templin, Chris <ctemplin@gmail.com>
2021-08-21 14:37:30 -0400

    About app page improvments. Adding scroll expansion animations. Adding "background" code sample with parallax scrolling.

--- pages/app-history/index.js
+++ pages/app-history/index.js
@@ -2,6 +2,7 @@ import * as fetch from 'node-fetch'
 import HeadTag from '../../components/head'
 import Image from 'next/image'
 import styles from '../../styles/AppHistory.module.sass'
+import { useEffect, useRef } from 'react'
 
 export async function getStaticProps(context) {
   const url = new URL(
@@ -38,47 +39,94 @@ export async function getStaticProps(context) {
 }
 
 export default function AppHistory({title, deploys}) {
+
+  const st = useRef(0)
+  useEffect(() => {
+    const onBgScroll = (e) => {
+      e.preventDefault() // allow bubble
+    }
+
+    const onScroll = (e,) => {
+      if (e.eventPhase !== 2) return
+
+      var dir = e.srcElement.scrollTop < st.current ? 'up' : 'down'
+      var codeBlocks = document.querySelectorAll('.' + styles.codeBg)
+      codeBlocks.forEach((el) => el.scrollBy(0, dir === 'up' ? -5 : 5))
+
+      if (e.srcElement.scrollTop > st.current) {
+        var toolBlocks = document.querySelectorAll('.' + styles.toolBlock)
+        // console.log(toolBlocks.length)
+        toolBlocks.forEach((el) => {
+          var n = el.offsetTop - document.body.scrollTop
+          if (n < 550) {
+            el.classList.add(styles.toolBlockFocus)
+          }
+        })
+      }
+      
+      st.current = e.srcElement.scrollTop
+    }
+    document.body.addEventListener('scroll', onScroll)
+    document.querySelectorAll('.' + styles.codeBg).forEach(
+      (el) => el.addEventListener('scroll', onBgScroll)
+    )
+    return () => {
+      document.body.removeEventListener('scroll', onScroll)
+      document.querySelectorAll('.' + styles.codeBg).forEach(
+        (el) => el.removeEventListener('scroll', onBgScroll)
+      )
+    }
+  }, [])
+
+  const pre =
+`useEffect(() => {
+  setCoverArt({id: dispData.id})
+  resetRecording()
+},[dispData.id, setCoverArt, resetRecording])`
+
   return (
     <>
     <HeadTag title={title} />
     <div className={styles.container}>
       <div className={styles.column}>
-        <ul>
-        <li>Packages: Nextjs, Recoiljs, Fontawesome, Sass</li>
-        <li>APIs: Musicbrainz, CoverArtArchive, Youtube, Netlify-API</li>
-        <li>Hosting: Netlify, Railway</li>
-        </ul>
 
+        <h1>Header</h1>
+        <p>Consectetur laboris non velit cupidatat consequat sit aliquip do aliqua laboris. Sunt quis veniam exercitation quis deserunt commodo fugiat nisi laborum. Consequat officia eiusmod aliqua eu. Aute duis esse ipsum nulla enim ad enim. Esse eiusmod minim cillum nisi quis cillum incididunt nisi eu ipsum anim laborum laboris. Cillum exercitation pariatur nisi cillum nostrud irure officia exercitation. Non sit voluptate ad exercitation.</p>
+
+        <p>Quis aliqua culpa consequat irure consequat. Ut ea aliqua anim adipisicing. Mollit labore reprehenderit eu do.</p>
         <h2 className={styles.category}>Packages</h2>
         <div className={styles.toolBlock} id="nextjsBlock">
           <div className={styles.toolLogo}>
             <Image
                 src="/nextjs_logo.png" className={styles.logo}
-                layout="intrinsic" alt="NextJs Logo"
+                layout="fixed" alt="NextJs Logo"
                 width="360" height="190"
               />
           </div>
-          <div className={styles.text}><h3>NextJS</h3> - Consectetur ea pariatur 
-          qui tempor et exercitation reprehenderit aute anim fugiat deserunt. 
-          Tempor laboris magna sunt sunt id excepteur labore. Commodo Lorem mollit 
-          in irure velit nulla ex.</div>
+          <div className={styles.text}>NextJS is React for "static" sites. This allows for on-demand, CDN-based deployments. Most of the dynamic data used in this app is requested directly by the browser (<a href="#APIs">see APIs</a> for MusicBraninz). Other API calls (Youtube, etc.) require private credentials. Such API calls are hosted as on-demand server functions.
+
+
+          </div>
+        </div>
+        <div className={styles.codeBg}>
+          <pre>{pre}{pre}{pre}{pre}{pre}{pre}{pre}{pre}{pre}{pre}{pre}{pre}{pre}{pre}{pre}{pre}</pre>
         </div>
         <div className={`${styles.toolBlock} ${styles.toolBlockAlt}`}>
           <div className={styles.toolLogo}>
             <Image
                 src="/recoiljs_logo.png" className={styles.logo}
-                layout="intrinsic" alt="RecoilJs Logo"
+                layout="fixed" alt="RecoilJs Logo"
                 width="375" height="125"
               />
           </div>
-          <div className={styles.text}><h3>Recoil</h3> -</div>
+          <div className={styles.text}><h3>Recoil</h3> </div>
         </div>
-        <h2 className={styles.category}>APIs</h2>
+        <h2 className={styles.category} id="APIs">APIs</h2>
         <div className={styles.toolBlock}>
           <div className={styles.toolLogo}>
             <Image
                 src="/musicbrainz_logo.png" className={styles.logo}
-                layout="intrinsic" alt="Musicbrainz Logo"
+                layout="fixed" alt="Musicbrainz Logo"
                 width="360" height="106"
               />
           </div>
@@ -88,23 +136,23 @@ export default function AppHistory({title, deploys}) {
           <div className={styles.toolLogo}>
             <Image
                 src="/netlify_logo.png" className={styles.logo}
-                layout="intrinsic" alt="Netlify Logo"
+                layout="fixed" alt="Netlify Logo"
                 width="368" height="100"
               />
           </div>
           <div className={`${styles.text} ${styles.faText}`}><h3>Netlify API</h3></div>
         </div>
-        {/* <h2 className={styles.category}>Design</h2>
+        <h2 className={styles.category}>Design</h2>
         <div className={styles.toolBlock}>
-          <div className={`${styles.text} ${styles.faText}`}><h3>Text about FontAwesome</h3></div>
           <div className={styles.toolLogo}>
             <Image
-                src="/musicbrainz_logo.png" className={styles.logo}
-                layout="intrinsic" alt="Musicbrainz Logo"
-                width="240" height="240"
+                src="/fontawesome_logo.png" className={styles.logo}
+                layout="fixed" alt="FontAwesome Logo"
+                width="400" height="80"
               />
           </div>
-        </div> */}
+          <div className={`${styles.text} ${styles.faText}`}><h3>FontAwesome</h3></div>
+        </div>
 
         {deploys.map(_=>
           <div className={styles.deployRow} key={_.id}>
