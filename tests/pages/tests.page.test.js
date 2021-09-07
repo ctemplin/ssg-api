b5c53ec9684de482b9a993e0f6af2ca9ee05ff2a
Templin, Chris <ctemplin@gmail.com>
2021-09-07 09:40:19 -0400

    Nerfing scratchpad test.

--- tests/pages/tests.page.test.js
+++ tests/pages/tests.page.test.js
@@ -6,21 +6,5 @@ describe('Non-production page for testing various libraries and features.', () =
 
     const { container } = render(<TestPage />)
 
-    const headers = screen.getAllByRole('heading', {level: 3})
-    expect(headers[0].textContent).toBe('secondary primary')
-    expect(headers[1].textContent).toBe('primary')
-    expect(headers[2].textContent).toBe('secondary')
-
-    expect(screen.getByText('Countries')).toBeVisible()
-    const releaseCountries = screen.getByRole('list')
-    // const countries = within(releaseCountries).getAllByRole('listitem')
-    const liUs = within(releaseCountries).queryByLabelText('US')
-    expect(liUs).toBeVisible()
-    expect(liUs).toBeChecked()
-    const liCa = within(releaseCountries).queryByLabelText('CA')
-    expect(liCa).toBeVisible()
-    expect(liCa).not.toBeChecked()
-    const liNull = within(releaseCountries).queryByLabelText('ZZ')
-    expect(liNull).toBeNull()
   })
 })
