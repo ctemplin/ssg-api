d225beda044aaed363de072a1a59b5cab23997a6
Templin, Chris <ctemplin@gmail.com>
2021-09-10 16:24:06 -0400

    Whitespace fixes.

--- tests/components/withStateMgmt.js
+++ tests/components/withStateMgmt.js
@@ -8,47 +8,46 @@ import withMbz from '../../components/mbzComponent'
 
 const withStateMgmt = (InnerComponent) => {
   const OuterComponent = ({id}) => {
-  const Artist_MBZ = withMbz(InnerComponent)
-  const ReleaseGroup_MBZ = withMbz(InnerComponent)
-  const Release_MBZ = withMbz(InnerComponent)
-  const resetThenSet = useSetRecoilState(resetThenSetValue)
+    const Artist_MBZ = withMbz(InnerComponent)
+    const ReleaseGroup_MBZ = withMbz(InnerComponent)
+    const Release_MBZ = withMbz(InnerComponent)
+    const resetThenSet = useSetRecoilState(resetThenSetValue)
 
-  useEffect(() => {
-    // Loading (API) JSON thru data layer with msw
-    if (InnerComponent.name == "Artist") {
-      resetThenSet({atom: currentArtistAtom, id: id})
-    }
-    if (InnerComponent.name == "Release") {
-      resetThenSet({atom: currentReleaseAtom, id: id})
-    }
-    if (InnerComponent.name == "ReleaseGroup") {
-      resetThenSet({atom: currentReleaseGroupAtom, id: id})
-    }
-  }, [])
+    useEffect(() => {
+      // Loading (API) JSON thru data layer with msw
+      if (InnerComponent.name == "Artist") {
+        resetThenSet({atom: currentArtistAtom, id: id})
+      }
+      if (InnerComponent.name == "Release") {
+        resetThenSet({atom: currentReleaseAtom, id: id})
+      }
+      if (InnerComponent.name == "ReleaseGroup") {
+        resetThenSet({atom: currentReleaseGroupAtom, id: id})
+      }
+    }, [])
 
-  switch (InnerComponent.name) {
-    case 'Artist':
-      return <Artist_MBZ
-        lookup={artistLookup}
-        atom={currentArtistAtom}
-        dispSel={currentArtistPanelFormatSorted}
-        dispParams={{column: 'default', dir: 'asc'}}
-      ><InnerComponent/></Artist_MBZ>
-
-    case 'ReleaseGroup':
-      return <ReleaseGroup_MBZ
+    switch (InnerComponent.name) {
+      case 'Artist':
+        return <Artist_MBZ
+          lookup={artistLookup}
+          atom={currentArtistAtom}
+          dispSel={currentArtistPanelFormatSorted}
+          dispParams={{column: 'default', dir: 'asc'}}
+        ><InnerComponent/></Artist_MBZ>
+      case 'ReleaseGroup':
+        return <ReleaseGroup_MBZ
           lookup={releaseGroupLookup}
           atom={currentReleaseGroupAtom}
           dispSel={currentReleaseGroupPanelFormat}
         ><InnerComponent /></ReleaseGroup_MBZ>
-    case 'Release':
-      return <Release_MBZ
-        lookup={releaseLookup}
-        atom={currentReleaseAtom}
-        dispSel={currentReleasePanelFormat}
-      ><InnerComponent/></Release_MBZ>
+      case 'Release':
+        return <Release_MBZ
+          lookup={releaseLookup}
+          atom={currentReleaseAtom}
+          dispSel={currentReleasePanelFormat}
+        ><InnerComponent/></Release_MBZ>
+    }
   }
-}
   OuterComponent.displayName = `WithStateMgmt(${InnerComponent.name})`
   return OuterComponent
 }
